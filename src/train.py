cc7c70b6ba1553f17e3b67d27ee362de75d829b6
almamatr2141 <almamatr2141@gmail.com>
2024-12-19 06:06:33 +0300

    Added Tensorboard

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 from model import AutoEncoder_Dynamics
 from dataset import NumpyCsvDataSet
-# from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 import torch
 import numpy as np
 import time
@@ -32,7 +32,7 @@ optimizer = torch.optim.Adam(enc_dyn_net.parameters(), lr=lr)
 
 expt_prefix = 'AutoEncoderDynamics-Training-'
 expt_name = expt_prefix + time.strftime('%m-%d-%H-%M-%S')
-# writer = SummaryWriter('runs/' + expt_name)
+writer = SummaryWriter('runs/' + expt_name)
 
 def train_one_epoch(model, epoch):
     model.train()
@@ -54,14 +54,14 @@ def train_one_epoch(model, epoch):
         optimizer.step()
         
         running_loss += np.array([total_loss.item(), predict_loss_G.item(), predict_loss_L2.item(), recon_loss.item()])
-        if i % 100 == 99:
+        if i % 100 == 0:
             avg_loss = running_loss / 100
             print ('[%d, %5d] loss: %.3f' % (epoch+1, i+1, avg_loss[0]))
-            # index = epoch * len(dyn_training_loader) + i
-            # writer.add_scalar('training_loss', avg_loss[0], index)
-            # writer.add_scalar('predict_loss_G', avg_loss[1], index)
-            # writer.add_scalar('predict_loss_L2', avg_loss[2], index)
-            # writer.add_scalar('recon_loss', avg_loss[3], index)
+            index = epoch * len(dyn_training_loader) + i
+            writer.add_scalar('training_loss', avg_loss[0], index)
+            writer.add_scalar('training_loss_G', avg_loss[1], index)
+            writer.add_scalar('training_loss_L2', avg_loss[2], index)
+            writer.add_scalar('training_recon_loss', avg_loss[3], index)
             running_loss[:] = 0.0
             
             
@@ -81,16 +81,16 @@ def test_one_epoch(model, epoch):
         total_loss, predict_loss_G, predict_loss_L2, recon_loss = enc_dyn_net.compute_loss(u_t, x_full, z_t, z_tplus, x_hat_full, z_hat_tplus, l2_weight)
                 
         running_loss += np.array([total_loss.item(), predict_loss_G.item(), predict_loss_L2.item(), recon_loss.item()])
-        if i % 100 == 99:
+        if i % 100 == 0:
             avg_loss = running_loss / 100
             print ('[%d, %5d] test loss: %.3f' % (epoch+1, i+1, avg_loss[0]))
-            # index = epoch * len(dyn_test_loader) + i
-            # writer.add_scalar('training_loss', avg_loss[0], index)
-            # writer.add_scalar('predict_loss_G', avg_loss[1], index)
-            # writer.add_scalar('predict_loss_L2', avg_loss[2], index)
-            # writer.add_scalar('recon_loss', avg_loss[3], index)
+            index = epoch * len(dyn_training_loader) + i
+            writer.add_scalar('testing_loss', avg_loss[0], index)
+            writer.add_scalar('testing_loss_G', avg_loss[1], index)
+            writer.add_scalar('testing_loss_L2', avg_loss[2], index)
+            writer.add_scalar('testing_recon_loss', avg_loss[3], index)
             running_loss[:] = 0.0
-
+            
 
 for epoch in tqdm(range(epochs)):
     train_one_epoch(enc_dyn_net, epoch)
